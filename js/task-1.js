7c907dce3bac4b25b2eba8bd2b8c0a9deb04e67b
AnyaRybalka <litra215@gmail.com>
2023-10-30 00:26:31 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,6 @@
 function makeTransaction(quantity, pricePerDroid) {
-    consolele.log(`You order ${quantity} droids worth ${totalPrice} credits!`)
+    const totalPrice = quantity * pricePerDroid;
+    return `"You order ${quantity} droids worth ${totalPrice} credits!"`
 }
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
